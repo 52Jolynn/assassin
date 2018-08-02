68dacdf8bc11e5ca7ebc1027adbbe27332aae6b5
huangtiande <huangtiande@rfchina.com>
2018-08-02 21:42:09 +0800

    feat: 搭建web框架

--- src/52jolynn.com/main.go
+++ src/52jolynn.com/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/kataras/iris"
+)
 
-func main()  {
-	fmt.Println("Hello World")
+func main() {
+	app := iris.Default()
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{
+			"message": "pong",
+		})
+	})
+	// Listen and serve on http://localhost:8080.
+	app.Run(iris.Addr(":8080"))
 }
